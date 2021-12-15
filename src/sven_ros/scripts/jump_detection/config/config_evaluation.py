2cb48bd083859cfb02432a2868e0ca9ba8516f8c
suitendaal <sven.uitendaal1@gmail.com>
2021-12-15 17:29:22 +0100

    trajectory with wiping out whiteboard

--- src/sven_ros/scripts/jump_detection/config/config_evaluation.py
+++ src/sven_ros/scripts/jump_detection/config/config_evaluation.py
@@ -17,31 +17,36 @@ baseline = [
 ]
 
 executions = [
-	'data/replay5.bag',
-	'data/replay6.bag',
-	'data/replay7.bag',
-	'data/replay8.bag',
-	'data/replay9.bag',
-	'data/replay10.bag',
-	'data/replay11.bag',
-	'data/replay12.bag',
-	'data/replay13.bag',
-	'data/replay14.bag',
-	'data/replay15.bag',
-	'data/replay16.bag',
-	'data/replay17.bag',
-	'data/replay18.bag',
-	'data/replay19.bag',
-	'data/replay20.bag',
-	'data/replay21.bag',
-	'data/replay22.bag',
-	'data/replay23.bag',
-	'data/replay24.bag',
-	'data/replay25.bag',
-	'data/replay26.bag',
-	'data/replay27.bag',
-	'data/replay28.bag'
+	'data/replay_test.bag',
+	'data/replay_test2.bag'
 ]
+
+#executions = [
+#	'data/replay5.bag',
+#	'data/replay6.bag',
+#	'data/replay7.bag',
+#	'data/replay8.bag',
+#	'data/replay9.bag',
+#	'data/replay10.bag',
+#	'data/replay11.bag',
+#	'data/replay12.bag',
+#	'data/replay13.bag',
+#	'data/replay14.bag',
+#	'data/replay15.bag',
+#	'data/replay16.bag',
+#	'data/replay17.bag',
+#	'data/replay18.bag',
+#	'data/replay19.bag',
+#	'data/replay20.bag',
+#	'data/replay21.bag',
+#	'data/replay22.bag',
+#	'data/replay23.bag',
+#	'data/replay24.bag',
+#	'data/replay25.bag',
+#	'data/replay26.bag',
+#	'data/replay27.bag',
+#	'data/replay28.bag'
+#]
 
 ### Output file
 output_plot_all = 'figures_git/all_evaluations.pdf'
